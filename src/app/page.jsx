4ab8f4cf6e0e342071f6147f5ce45a667b098127
Landensaki <gededewowahyumw@gmail.com>
2023-06-21 11:54:48 +0700

    Tailwind Class Sorting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,15 +20,15 @@ export default function Login() {
   };
 
   return (
-    <main className="bg-[#2f3349] w-screen h-screen flex items-center justify-center">
-      <div className="w-[450px] h-[450px] rounded-xl flex flex-col justify-center items-center">
+    <main className="flex h-screen w-screen items-center justify-center bg-[#2f3349]">
+      <div className="flex h-[450px] w-[450px] flex-col items-center justify-center rounded-xl">
         {/* TITLE */}
-        <p className="text-3xl font-bold mb-8 text-white">-- INVENTORY --</p>
+        <p className="mb-8 text-3xl font-bold text-white">-- INVENTORY --</p>
         {/* TITLE */}
 
-        <form onSubmit={authentication} className="flex w-80 flex-col gap-y-3 justify-center items-center">
+        <form onSubmit={authentication} className="flex w-80 flex-col items-center justify-center gap-y-3">
           {/* INPUT EMAIL */}
-          <div className="relative mt-6 flex flex-col w-full justify-center text-white">
+          <div className="relative mt-6 flex w-full flex-col justify-center text-white">
             <input
               type="email"
               name="email"
@@ -36,7 +36,7 @@ export default function Login() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="w-full peer/txtemail h-10 px-3 rounded-md border-2 border-white bg-transparent outline-none focus:border-[#7367f0]"
+              className="peer/txtemail h-10 w-full rounded-md border-2 border-white bg-transparent px-3 outline-none focus:border-[#7367f0]"
             />
             <label htmlFor="email" className="absolute -top-6 peer-focus/txtemail:text-[#7367f0]">
               Email
@@ -45,7 +45,7 @@ export default function Login() {
           {/* INPUT EMAIL */}
 
           {/* INPUT PASSWORD */}
-          <div className="relative mt-6 flex flex-col justify-center w-full text-white">
+          <div className="relative mt-6 flex w-full flex-col justify-center text-white">
             <input
               type="password"
               name="password"
@@ -53,7 +53,7 @@ export default function Login() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="w-full peer/txtpassword h-10 px-3 rounded-md border-2 border-white bg-transparent outline-none focus:border-[#7367f0]"
+              className="peer/txtpassword h-10 w-full rounded-md border-2 border-white bg-transparent px-3 outline-none focus:border-[#7367f0]"
             />
             <label htmlFor="password" className="absolute -top-6 peer-focus/txtpassword:text-[#7367f0]">
               Password
@@ -62,11 +62,11 @@ export default function Login() {
           {/* INPUT PASSWORD */}
 
           {/* INDICATOR */}
-          <p className="text-red-500 text-lg my-2">{indicator}</p>
+          <p className="my-2 text-lg text-red-500">{indicator}</p>
           {/* INDICATOR */}
 
           {/* SUBMIT BUTTON */}
-          <button type="submit" className="font-bold bg-[#7367f0] text-white hover:bg-[#7367f0]/70 w-full h-11 rounded-md">
+          <button type="submit" className="h-11 w-full rounded-md bg-[#7367f0] font-bold text-white hover:bg-[#7367f0]/70">
             LOGIN
           </button>
           {/* SUBMIT BUTTON */}
