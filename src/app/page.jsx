3043ae340b8f5e2265789a560ee2720eab1e8daa
Landensaki <gededewowahyumw@gmail.com>
2023-06-22 22:09:52 +0700

    Add GET Account Data API For Login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,80 +1,20 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
+import Login from "@/components/login/Login";
 
-export default function Login() {
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [indicator, setIndicator] = useState("");
+async function getAccount() {
+  const res = await fetch("http://localhost:5000/account", {
+    cache: "no-store",
+  });
+  return res.json();
+}
 
-  const authentication = (e) => {
-    e.preventDefault();
-    if (email === "test@gmail.com" && password === "123456") {
-      router.push("/test");
-    } else {
-      setIndicator("Invalid Email or Password");
-      setPassword("");
-    }
-  };
+export default async function Page() {
+  const accountData = await getAccount();
 
   return (
-    <main className="flex h-screen w-screen items-center justify-center bg-[#2f3349]">
-      <div className="flex h-[450px] w-[450px] flex-col items-center justify-center rounded-xl">
-        {/* TITLE */}
-        <p className="mb-8 text-3xl font-bold text-white">-- INVENTORY --</p>
-        {/* TITLE */}
-
-        <form onSubmit={authentication} className="flex w-80 flex-col items-center justify-center gap-y-3">
-          {/* INPUT EMAIL */}
-          <div className="relative mt-6 flex w-full flex-col justify-center text-white">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="peer/txtemail h-10 w-full rounded-md border-2 border-white bg-transparent px-3 outline-none focus:border-[#7367f0]"
-            />
-            <label htmlFor="email" className="absolute -top-6 peer-focus/txtemail:text-[#7367f0]">
-              Email
-            </label>
-          </div>
-          {/* INPUT EMAIL */}
-
-          {/* INPUT PASSWORD */}
-          <div className="relative mt-6 flex w-full flex-col justify-center text-white">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="peer/txtpassword h-10 w-full rounded-md border-2 border-white bg-transparent px-3 outline-none focus:border-[#7367f0]"
-            />
-            <label htmlFor="password" className="absolute -top-6 peer-focus/txtpassword:text-[#7367f0]">
-              Password
-            </label>
-          </div>
-          {/* INPUT PASSWORD */}
-
-          {/* INDICATOR */}
-          <p className="my-2 text-lg text-red-500">{indicator}</p>
-          {/* INDICATOR */}
-
-          {/* SUBMIT BUTTON */}
-          <button type="submit" className="h-11 w-full rounded-md bg-[#7367f0] font-bold text-white hover:bg-[#7367f0]/70">
-            LOGIN
-          </button>
-          {/* SUBMIT BUTTON */}
-        </form>
-
-        {/* COPYRIGHT */}
-        <p className="mt-10 select-none text-gray-400">&copy; Copyright 2023</p>
-        {/* END COPYRIGHT */}
-      </div>
-    </main>
+    <Login
+      getEmail={accountData[0].email}
+      getPassword={accountData[0].password}
+    />
   );
 }
