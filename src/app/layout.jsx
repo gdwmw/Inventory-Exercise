a93d5c752c35406e4c7640096e764c0839645868
gdwmw <gededewowahyumw@gmail.com>
2023-06-26 01:51:37 +0700

    Fix Root Layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=0" />
+        {/* <link rel="icon" type="image/x-icon" href="/favicon.ico" /> */}
+      </head>
       <body>{children}</body>
     </html>
   );
